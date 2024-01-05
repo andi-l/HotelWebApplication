2b90ca208f7dd1edb20232c7448927b5e3ef9cd5
andi-l <andi.latifi@stud.fra-uas.de>
2024-01-05 19:33:32 +0100

    Small changes

--- hotel/src/main/java/tech/titans/hotel/Service/RatingServiceInterface.java
+++ hotel/src/main/java/tech/titans/hotel/Service/RatingServiceInterface.java
@@ -2,8 +2,6 @@ package tech.titans.hotel.Service;
 
 import tech.titans.hotel.Model.Rating;
 
-import java.util.ArrayList;
-
 public interface RatingServiceInterface {
 
     public void addRating(Rating rating);
