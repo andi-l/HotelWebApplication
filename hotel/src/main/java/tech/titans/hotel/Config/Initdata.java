7dc8baab38c56123b3d9d6731ac4e11bbbee8970
Lenny <111445712+Dobbell@users.noreply.github.com>
2024-01-07 18:07:35 +0100

    Updated initData

--- hotel/src/main/java/tech/titans/hotel/Config/Initdata.java
+++ hotel/src/main/java/tech/titans/hotel/Config/Initdata.java
@@ -1,3 +1,45 @@
 package tech.titans.hotel.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
 
+import tech.titans.hotel.HotelApplication;
+import tech.titans.hotel.Model.Rating;
+import tech.titans.hotel.Repository.RatingRepository;
+import tech.titans.hotel.Service.RatingService;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import tech.titans.hotel.Repository.*;
+
+
+@Component
+public class InitData {
+
+    @Autowired
+    RatingRepository ratingRepository;
+
+    @Autowired
+    RatingService ratingService;
+    
+    
+    @PostConstruct
+    public void init() {
+
+        
+        Rating rating1 = new Rating(3, "hallo";)
+
+
+
+
+
+
+        
+        
+}
+}
