b9a9f9ca639b8484e738a57de44225c898358b6e
andi-l <andi.latifi@stud.fra-uas.de>
2024-01-31 15:51:55 +0100

    ratingservice module

---
+++ hotel/ratingservice/src/main/java/fra/uas/RatingserviceApplication.java
@@ -0,0 +1,13 @@
+package fra.uas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingserviceApplication.class, args);
+    }
+
+}
